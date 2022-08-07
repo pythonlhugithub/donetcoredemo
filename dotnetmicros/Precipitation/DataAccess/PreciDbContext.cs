dabffcaeb61529312ed6884cd275dedbca673192
david lizhong huang <davidlizhonghuang@users.noreply.github.com>
2022-08-07 12:51:43 +1000

    add dbcontect

--- dotnetmicros/Precipitation/DataAccess/PreciDbContext.cs
+++ dotnetmicros/Precipitation/DataAccess/PreciDbContext.cs
@@ -11,7 +11,24 @@ public PreciDbContext(DbContextOptions opts): base(opts){
 
 }
 public DbSet<Precipitation> precipitation {get;set;}
- 
+
+ protected override void OnModelCreating (ModelBuilder modelBuilder){
+base.OnModelCreating(modelBuilder);
+snakeIdentityTableNames(modelBuilder);
 }
+
+public void snakeIdentityTableNames(ModelBuilder nm){
+nm.Entity<Precipitation>(b=>{b.ToTable("precipitation");});
+}
+
+
+protected override void OnConfiguring(DbContextOptionsBuilder options)=>
+    options.UseSqlServer($"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=weatherDb;Data Source=.\\sqlexpress");
+
+
+
+}
+
+
 
 }
---
+++ dotnetmicros/Precipitation/Migrations/20220807024351_initCreating.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Precipitation.Migrations
+{
+    public partial class initCreating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "precipitation",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amountinches = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    WeatherType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ZipCOde = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_precipitation", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "precipitation");
+        }
+    }
+}
